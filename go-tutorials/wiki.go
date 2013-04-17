3350319c5c75e218f5afa14e25a0d542073c36b1
Andy Sayler <andy.sayler@gmail.com>
2013-04-16 20:57:04 -0600

    Added go tutorial

---
+++ go-tutorials/wiki.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
