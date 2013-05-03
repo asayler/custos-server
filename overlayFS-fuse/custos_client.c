47b1598da5e3004f22f6dfce0651784f89ccc9c9
Andy Sayler <andy.sayler@gmail.com>
2013-05-02 22:46:16 -0600

    Minor refactoring and some more implmentation

--- overlayFS-fuse/custos_client.c
+++ overlayFS-fuse/custos_client.c
@@ -12,6 +12,42 @@
 #define RETURN_FAILURE -1
 #define RETURN_SUCCESS  0
 
+extern custosReq_t* custos_createReq(const custosUUID_t* uuid) {
+
+    custosReq_t* req = NULL;
+
+
+    if(!uuid) {
+#ifdef DEBUG
+	fprintf(stderr, "ERROR custos_createReq: 'uuid' must not be NULL\n");
+#endif
+	return NULL;
+    }
+
+    req = malloc(sizeof(*req));
+    if(!req) {
+#ifdef DEBUG
+	fprintf(stderr, "ERROR custos_createReq: malloc failed\n");
+	perror(         "---------------------->");
+#endif
+	return NULL;
+    }
+
+#ifdef DEBUG
+    fprintf(stderr, "INFO custos_createReq: uuid memcpy size = %z\n",
+	    sizeof(req->uuid));
+#endif
+    memcpy(&(req->uuid), uuid, sizeof(req->uuid));
+#ifdef DEBUG
+    fprintf(stderr, "INFO custos_createReq: attrs memset size = %z\n",
+	    (sizeof(custosAttr_t) * sizeof(req->attrs)));
+#endif
+    memset(req->attrs, 0, (sizeof(custosAttr_t) * sizeof(req->attrs)));
+
+    return req;
+
+}
+
 extern int custos_getkey(const custosReq_t* req, custosRes_t* res) {
 
     (void) req;
--- overlayFS-fuse/custos_client.h
+++ overlayFS-fuse/custos_client.h
@@ -13,16 +13,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <errno.h>
 #include <inttypes.h>
 
+typedef struct custosUUID {
+    uint8_t bytes[16];
+} custosUUID_t;
+
+typedef struct custosAttr {
+    void*  val;
+    size_t size;
+} custosAttr_t;
+
+typedef enum custosAttrID {
+    CUS_ATTRID_PSK = 0,
+    CUS_ATTRID_MAX,
+} custosAttrID_t;
+
 typedef struct custosReq {
-    uint8_t uuid[16];
-    char*   psk;
+    custosUUID_t uuid;
+    custosAttr_t attrs[CUS_ATTRID_MAX];
 } custosReq_t;
 
 typedef struct custosRes {
     uint8_t* key;
 } custosRes_t;
+
+extern custosReq_t* custos_createReq(const custosUUID_t* uuid);
+extern int custos_updateReq(custosReq_t* req, const custosAttrID_t id,
+			    const void* value, const size_t size);
+extern int custos_destroyReq(custosReq_t** req);
 
 extern int custos_getkey(const custosReq_t* req, custosRes_t* res);
 
