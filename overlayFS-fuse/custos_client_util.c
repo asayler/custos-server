1f8167d06e05feb17775960b271313b6f1826b3c
Andy Sayler <andy.sayler@gmail.com>
2013-05-03 00:08:25 -0600

    Added custos client util stub

---
+++ overlayFS-fuse/custos_client_util.c
@@ -0,0 +1,23 @@
+/* custos_client_util.c
+ * 
+ * custos_client utility tool
+ *
+ * By Andy Sayler (www.andysayler.com)
+ * Created  05/02/13
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "custos_client.h"
+
+int main(int argc, char* argv[]) {
+
+    (void) argc;
+    (void) argv;
+
+    return EXIT_SUCCESS;
+
+}
