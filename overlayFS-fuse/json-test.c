115d9bfbd8b128452db7820a572e8cfe27dfada8
Andy Sayler <andy.sayler@gmail.com>
2013-05-05 22:37:18 -0600

    Added json-test stub

---
+++ overlayFS-fuse/json-test.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <json/json.h>
+
+int main(int argc, char* argv[]) {
+
+    (void) argc;
+    (void) argv;
+
+    return EXIT_SUCCESS;
+
+}
