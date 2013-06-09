2a1482e50ad73cd77b64b836a020c7f3577c9bdc
Andy Sayler <andy.sayler@gmail.com>
2013-06-09 17:03:17 -0600

    Finished json-test example

--- overlayFS-fuse/json-test.c
+++ overlayFS-fuse/json-test.c
@@ -4,66 +4,70 @@
 #include <json/json.h>
 
 /*Parsing the json object*/
-void json_parse(json_object* jobj) {
+void json_print(json_object* jobj, int offset) {
 
-    json_object_iter iter;
-
-    json_object_object_foreachC(jobj, iter) {
-
-	const json_type type = json_object_get_type(iter.val);
-	const char* sType = json_type_to_name(type);
-	const char* sKey  = iter.key;
-
-	printf("type: %s\n", sType);
-
-	switch(type) {
-	case json_type_boolean:
-	    {
-		const int val = json_object_get_boolean(iter.val);
-		printf("key:  %s\n", sKey);
-		printf("val:  %d\n", val);
-	    }
-	    break;
-	case json_type_double:
-	    {
-		const double val = json_object_get_double(iter.val);
-		printf("key:  %s\n", sKey);
-		printf("val:  %f\n", val);
-	    }
-	    break;
-	case json_type_int:
-	    {
-		const int val = json_object_get_int(iter.val);
-		printf("key:  %s\n", sKey);
-		printf("val:  %d\n", val);
-	    }
-	    break;
-	case json_type_string:
-	    {
-		const char* val = json_object_get_string(iter.val);
-		printf("key:  %s\n", sKey);
-		printf("val:  %s\n", val);
-	    }
-	    break;
-	case json_type_object:
-	    {
-		jobj = json_object_object_get(jobj, iter.key);
-		printf("key:  %s\n", sKey);
-		json_parse(jobj);
-	    }
-	    break;
-	case json_type_array:
-	    {
-		printf("key:  %s\n", sKey);
-	    }
-	    break;
-	case json_type_null:
-	    printf("WARNING - Null Type Detected/n");
-	    break;
-
+    const json_type type = json_object_get_type(jobj);
+    const char* sType = json_type_to_name(type);
+    //printf("%*s" "---\n", offset, "");
+    printf("%*s" "type: %s\n", offset, "", sType);
+    printf("%*s" "val:  ", offset, "");
+    
+    switch(type) {
+    case json_type_boolean:
+	{
+	    const int val = json_object_get_boolean(jobj);
+	    printf("%d\n", val);
 	}
-
+	break;
+    case json_type_double:
+	{
+	    const double val = json_object_get_double(jobj);
+	    printf("%f\n", val);
+	}
+	break;
+    case json_type_int:
+	{
+	    const int val = json_object_get_int(jobj);
+	    printf("%d\n", val);
+	}
+	break;
+    case json_type_string:
+	{
+	    const char* val = json_object_get_string(jobj);
+	    printf("%s\n", val);
+	}
+	break;
+    case json_type_object:
+	{
+	    json_object_iter iter;
+	    printf("\n");
+	    json_object_object_foreachC(jobj, iter) {
+		const char* sKey  = iter.key;
+		printf("%*s" "key:  %s\n", (offset +4), "", sKey);
+		json_print(iter.val, (offset + 4));
+	    }
+	}
+	break;
+    case json_type_array:
+	{
+	    int len;
+	    int i;
+	    len = json_object_array_length(jobj);
+	    printf("\n");
+	    for(i=0; i < len; i++) {
+		json_object* val = json_object_array_get_idx(jobj, i);
+		json_print(val, (offset + 4));
+	    }
+	}
+	break;
+    case json_type_null:
+	{
+	    printf("WARNING - Null Type Detected/n");
+	}
+	break;
     }
+
+    printf("%*s" "---\n", offset, "");
 
 }
 
@@ -73,20 +77,25 @@ int main(int argc, char* argv[]) {
     (void) argv;
 
     char* string = 
-	"{"
-	"\"sitename\" : "
-	"\"joys of programming\","
-	"\"categories\" : "
-	"[ \"c\" , [\"c++\" , \"c\" ], \"java\", \"PHP\" ],"
+	"{ "
+	"\"sitename\": "
+	"\"joys of programming\", "
+	"\"categories\": "
+	"[ \"c\", [ \"c++\", \"c\" ], \"java\", \"PHP\" ], "
 	"\"author-details\": "
-	"{ \"admin\": false, \"name\" : \"Joys of Programming\", \"Number of Posts\" : 10 }"
-	"}";
+	"{ \"admin\": false, \"name\": \"Joys of Programming\", \"Number of Posts\": 10 }"
+	" }";
 
-    printf("JSON string: %s\n", string);
-    
+    printf("input: %s\n", string);
+
     json_object* jobj = json_tokener_parse(string);
-    json_parse(jobj);
+
+    printf("jobj:  %s\n", json_object_to_json_string(jobj));
+    
+    json_print(jobj, 0);
         
+    json_object_put(jobj);
+
     return EXIT_SUCCESS;
 
 }
