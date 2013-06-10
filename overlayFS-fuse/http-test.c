1d2a3de6d715531d526be9ad1cddbeb76c7fd14c
Andy Sayler <andy.sayler@gmail.com>
2013-06-09 23:28:15 -0600

    Added http test and updated Makefile and gitignore

---
+++ overlayFS-fuse/http-test.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "http-util.h"
+
+#define URI "http://condor.andysayler.com/test.json"
+
+int main(int argc, char* argv[]) {
+
+    (void) argc;
+    (void) argv;
+
+    HttpData_t res;
+    long resCode;
+
+    /* Initialize Data */
+    res.size = 0; 
+    res.data = NULL;
+
+    /* Make Request */
+    resCode = httpGet(URI, &res);
+
+    /* Print Output */
+    fprintf(stdout, "resCode = %ld\n", resCode);
+    fprintf(stdout, "res.size = %zd\n", res.size);
+    if(res.data) {
+	fprintf(stdout, "res.data:\n%s\n", res.data);
+    }
+
+    /* Clean Up */
+    if(res.data) {
+    	free(res.data);
+    }
+
+    return EXIT_SUCCESS;
+}
