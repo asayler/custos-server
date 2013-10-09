3c00d67e93bdd8da968464dafe81a9f32923c9de
Andy Sayler <andy.sayler@gmail.com>
2013-10-08 21:27:20 -0600

    Added response print functions

--- overlayFS-fuse/custos_client_util.c
+++ overlayFS-fuse/custos_client_util.c
@@ -75,12 +75,41 @@ int custos_printAttrReq(custosAttrReq_t* attrreq, uint offset, FILE* stream) {
 
     fprintf(stream, "%*s" "attrreq->echo = %s\n", offset, "",
 	    attrreq->echo ? "true" : "false");
-    fprintf(stream, "%*s" "attrreq->attr  = %p\n", offset, "", attrreq->attr);
+    fprintf(stream, "%*s" "attrreq->attr = %p\n", offset, "", attrreq->attr);
     if(attrreq->attr) {
 	ret = custos_printAttr(attrreq->attr, (offset + CUS_PRINT_OFFSET), stream);
 	if(ret < 0) {
 #ifdef DEBUG
 	    fprintf(stderr, "ERROR custos_printAttrReq: custos_printAttr() failed\n");
+#endif
+	    return ret;
+	}
+    }
+
+    return RETURN_SUCCESS;
+
+}
+
+int custos_printAttrRes(custosAttrRes_t* attrres, uint offset, FILE* stream) {
+
+    int ret;
+
+    if(!attrres) {
+#ifdef DEBUG
+	fprintf(stderr, "ERROR custos_printAttrRes: 'attrres' must not be NULL\n");
+#endif
+	return -EINVAL;
+    }
+
+    fprintf(stream, "%*s" "attrres->status = %d\n", offset, "", attrres->status);
+    fprintf(stream, "%*s" "attrres->echo   = %s\n", offset, "",
+	    attrres->echo ? "true" : "false");
+    fprintf(stream, "%*s" "attrres->attr   = %p\n", offset, "", attrres->attr);
+    if(attrres->attr) {
+	ret = custos_printAttr(attrres->attr, (offset + CUS_PRINT_OFFSET), stream);
+	if(ret < 0) {
+#ifdef DEBUG
+	    fprintf(stderr, "ERROR custos_printAttrRes: custos_printAttr() failed\n");
 #endif
 	    return ret;
 	}
@@ -140,6 +169,35 @@ int custos_printKeyReq(custosKeyReq_t* keyreq, uint offset, FILE* stream) {
 
 }
 
+int custos_printKeyRes(custosKeyRes_t* keyres, uint offset, FILE* stream) {
+
+    int ret;
+
+    if(!keyres) {
+#ifdef DEBUG
+	fprintf(stderr, "ERROR custos_printKeyRes: 'key' must not be NULL\n");
+#endif
+	return -EINVAL;
+    }
+
+    fprintf(stream, "%*s" "keyres->status = %d\n", offset, "", keyres->status);
+    fprintf(stream, "%*s" "keyres->echo   = %s\n", offset, "",
+	    keyres->echo ? "true" : "false");
+    fprintf(stream, "%*s" "keyres->key  = %p\n", offset, "", keyres->key);
+    if(keyres->key) {
+	ret = custos_printKey(keyres->key, (offset + CUS_PRINT_OFFSET), stream);
+	if(ret < 0) {
+#ifdef DEBUG
+	    fprintf(stderr, "ERROR custos_printKeyRes: custos_printKey() failed\n");
+#endif
+	    return ret;
+	}
+    }
+
+    return RETURN_SUCCESS;
+
+}
+
 int custos_printReq(custosReq_t* req, uint offset, FILE* stream) {
 
     size_t i;
@@ -190,6 +248,67 @@ int custos_printReq(custosReq_t* req, uint offset, FILE* stream) {
 	    if(ret < 0) {
 #ifdef DEBUG
 		fprintf(stderr, "ERROR custos_printReq: custos_printKeyReq() failed\n");
+#endif
+		return ret;
+	    }
+	}
+    }
+
+    return RETURN_SUCCESS;
+
+}
+
+int custos_printRes(custosRes_t* res, uint offset, FILE* stream) {
+
+    size_t i;
+    uint newoffset = offset + CUS_PRINT_OFFSET;
+    int ret;
+
+    if(!res) {
+#ifdef DEBUG
+	fprintf(stderr, "ERROR custos_printRes: 'res' must not be NULL\n");
+#endif
+	return -EINVAL;
+    }
+    if(!res->source) {
+#ifdef DEBUG
+	fprintf(stderr, "ERROR custos_printRes: 'res->source' must not be NULL\n");
+#endif
+	return -EINVAL;
+    }
+    if(!res->version) {
+#ifdef DEBUG
+	fprintf(stderr, "ERROR custos_printRes: 'res->version' must not be NULL\n");
+#endif
+	return -EINVAL;
+    }
+
+    fprintf(stream, "%*s" "res->status    = %d\n",  offset, "", res->status);
+    fprintf(stream, "%*s" "res->source    = %s\n",  offset, "", res->source);
+    fprintf(stream, "%*s" "res->version   = %s\n",  offset, "", res->version);
+    fprintf(stream, "%*s" "res->num_attrs = %zd\n", offset, "", res->num_attrs);
+    for(i = 0; i < res->num_attrs; i++) {
+	fprintf(stream, "%*s" "res->attrs[%zd] = %p\n",
+		newoffset, "", i, res->attrs[i]);
+	if(res->attrs[i]) {
+	    ret = custos_printAttrRes(res->attrs[i], (newoffset + CUS_PRINT_OFFSET), stream);
+	    if(ret < 0) {
+#ifdef DEBUG
+		fprintf(stderr, "ERROR custos_printRes: custos_printAttrRes() failed\n");
+#endif
+		return ret;
+	    }
+	}
+    }
+    fprintf(stream, "%*s" "res->num_keys  = %zd\n", offset, "", res->num_keys);
+    for(i = 0; i < res->num_keys; i++) {
+	fprintf(stream, "%*s" "res->keys[%zd] = %p\n",
+		newoffset, "", i, res->keys[i]);
+	if(res->keys[i]) {
+	    ret = custos_printKeyRes(res->keys[i], (newoffset + CUS_PRINT_OFFSET), stream);
+	    if(ret < 0) {
+#ifdef DEBUG
+		fprintf(stderr, "ERROR custos_printRes: custos_printKeyRes() failed\n");
 #endif
 		return ret;
 	    }
