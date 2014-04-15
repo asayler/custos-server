0f688a4811c51b7fd963116acbdf146f68d36f50
Andy Sayler <andy.sayler@gmail.com>
2014-04-14 23:02:56 -0600

    Added row oriented storage class

--- custos-server/python/custos/datastore.py
+++ custos-server/python/custos/datastore.py
@@ -19,8 +19,10 @@ DS_OBJ_VAL = "ds_obj_val"
 DS_OBJ_VER_READ = "ds_obj_ver_read"
 DS_OBJ_VER_UPDATE = "ds_obj_ver_update"
 
-DS_AA = "ds_aa"
+DS_AA = 'ds_aa'
+DS_AA_ROW = { 'Class': None, 'Type': None, 'Value':None }
 
+DS_ROW_MAP = { DS_AA: DS_AA_ROW }
 
 class _DSbase(object):
     """
@@ -44,6 +46,10 @@ class _DSbase(object):
     def __delitem__(self, key):
         """Delete DS item"""
 
+    @abstractmethod
+    def get_row(self, row_id):
+        """Return DSrow item"""
+
 
 class _DSshelve(_DSbase):
     """
@@ -52,23 +58,71 @@ class _DSshelve(_DSbase):
     """
 
     def __init__(self, name):
+        """Setup DS"""
         super(_DSshelve, self).__init__(name)
         closing(shelve.open(self._name, 'c'))
 
     def __getitem__(self, key):
+        """Get DS item"""
         with closing(shelve.open(self._name, 'r')) as s:
             return s[key]
 
     def __setitem__(self, key, val):
+        """Set DS item"""
         with closing(shelve.open(self._name, 'w')) as s:
             s[key] = val
             return s[key]
 
     def __delitem__(self, key):
+        """Delete DS item"""
         with closing(shelve.open(self._name, 'w')) as s:
             del(s[key])
             return None
 
+    def get_row(self, row_id):
+        """Return DSrow item"""
+        row_proto = DS_ROW_MAP[self._name]
+        return DSrow(self, row_id, row_proto)
+
+class DSrow(object):
+    """
+    Custos database row
+
+    """
+
+    def __init__(self, ds, row_id, row_proto):
+        """Setup DS"""
+        self._ds = ds
+        self.row_id = row_id
+        self.prototype = row_proto
+        if self.row_id not in self._ds:
+            # Create prototype row
+            self._ds[row_id] = self.prototype
+
+    def __getitem__(self, key):
+        """Get DS row item"""
+        row = self.row()
+        return row[key]
+
+    def __setitem__(self, key, val):
+        """Set DS row item"""
+        row = self.row()
+        row[key] = val
+        self.set_row(row)
+        return row[key]
+
+    def __delitem__(self, key):
+        """Delete DS row item"""
+        pass
+
+    def get_row(self):
+        """Get DS row"""
+        return self._ds[row_id]
+
+    def set_row(self, row):
+        """Set DS row"""
+        self._ds[row_id] = row
+        return self._ds[row_id]
 
 class DS(_DSshelve):
     """
