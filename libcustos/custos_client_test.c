741c17b0a2f9bf6d044f931165df1ce935480357
Andy Sayler <andy.sayler@gmail.com>
2014-04-30 16:37:45 -0600

    Fixed spacing

--- libcustos/custos_client_test.c
+++ libcustos/custos_client_test.c
@@ -41,39 +41,39 @@ int main(int argc, char* argv[]) {
     /* Setup a new request */
     req = custos_createReq(SERVER_URL);
     if(!req) {
-	fprintf(stderr, "ERROR %s: custos_createKeyReq() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_createKeyReq() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     /* Add Key to Request */
     if(uuid_parse(UUID, uuid) < 0) {
-	fprintf(stderr, "ERROR %s: uuid_parse() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: uuid_parse() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     key = custos_createKey(uuid, 1, 0, NULL);
     if(!key) {
-	fprintf(stderr, "ERROR %s: custos_createKey() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_createKey() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     keyreq = custos_createKeyReq(true);
     if(!keyreq) {
-	fprintf(stderr, "ERROR %s: custos_createKeyReq() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_createKeyReq() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     if(custos_updateKeyReqAddKey(keyreq, key) < 0) {
-	fprintf(stderr, "ERROR %s: custos_updateKeyReqAddKey() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_updateKeyReqAddKey() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     if(custos_updateReqAddKeyReq(req, keyreq) < 0) {
-	fprintf(stderr, "ERROR %s: custos_updateReqAddKeyReq() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_updateReqAddKeyReq() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     /* Print Request */
     fprintf(stdout, "********* First  Request *********\n");
     if(custos_printReq(req, 0, stdout) < 0) {
-	fprintf(stderr, "ERROR %s: custos_printReq() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_printReq() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     fprintf(stdout, "\n");
 
@@ -81,13 +81,13 @@ int main(int argc, char* argv[]) {
     fprintf(stdout, "******* First  Request Json *******\n");
     reqjson = custos_ReqToJson(req);
     if(!reqjson) {
-	fprintf(stderr, "ERROR %s: custos_ReqToJson() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_ReqToJson() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     reqstr = json_object_to_json_string(reqjson);
     if(!reqstr) {
-	fprintf(stderr, "ERROR %s: json_object_to_json_string() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: json_object_to_json_string() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     fprintf(stdout, "%s\n", reqstr);
     fprintf(stdout, "\n");
@@ -104,43 +104,43 @@ int main(int argc, char* argv[]) {
     /* Print Response */
     fprintf(stdout, "********* First Response ********\n");
     if(custos_printRes(res, 0, stdout) < 0) {
-	fprintf(stderr, "ERROR %s: custos_printRes() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_printRes() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     fprintf(stdout, "\n");
 
     /* Free Response */
     if(custos_destroyRes(&res) < 0) {
-	fprintf(stderr, "ERROR %s: custos_destroyRes() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_destroyRes() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     /* Add attr to request */
     attr = custos_createAttr(CUS_ATTRCLASS_EXPLICIT, CUS_ATTRTYPE_EXP_PSK, 0,
-			     (strlen(BAD_PSK) + 1), (uint8_t*) BAD_PSK);
+                             (strlen(BAD_PSK) + 1), (uint8_t*) BAD_PSK);
     if(!attr) {
-	fprintf(stderr, "ERROR %s: custos_createAttr() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_createAttr() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     attrreq = custos_createAttrReq(true);
     if(!attrreq) {
-	fprintf(stderr, "ERROR %s: custos_createAttrReq() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_createAttrReq() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     if(custos_updateAttrReqAddAttr(attrreq, attr) < 0) {
-	fprintf(stderr, "ERROR %s: custos_updateAttrReqAddAttr() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_updateAttrReqAddAttr() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     if(custos_updateReqAddAttrReq(req, attrreq) < 0) {
-	fprintf(stderr, "ERROR %s: custos_updateReqAddAttrReq() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_updateReqAddAttrReq() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     /* Print Request */
     fprintf(stdout, "********* Second  Request *********\n");
     if(custos_printReq(req, 0, stdout) < 0) {
-	fprintf(stderr, "ERROR %s: custos_printReq() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_printReq() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     fprintf(stdout, "\n");
 
@@ -148,13 +148,13 @@ int main(int argc, char* argv[]) {
     fprintf(stdout, "******* Second  Request Json *******\n");
     reqjson = custos_ReqToJson(req);
     if(!reqjson) {
-	fprintf(stderr, "ERROR %s: custos_ReqToJson() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_ReqToJson() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     reqstr = json_object_to_json_string(reqjson);
     if(!reqstr) {
-	fprintf(stderr, "ERROR %s: json_object_to_json_string() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: json_object_to_json_string() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     fprintf(stdout, "%s\n", reqstr);
     fprintf(stdout, "\n");
@@ -171,30 +171,30 @@ int main(int argc, char* argv[]) {
     /* Print Response */
     fprintf(stdout, "********* Second Response ********\n");
     if(custos_printRes(res, 0, stdout) < 0) {
-	fprintf(stderr, "ERROR %s: custos_printRes() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_printRes() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     fprintf(stdout, "\n");
 
     /* Free Response */
     if(custos_destroyRes(&res) < 0) {
-	fprintf(stderr, "ERROR %s: custos_destroyRes() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_destroyRes() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     /* Update attr in request */
     if(custos_updateAttr(attr,
-			 CUS_ATTRCLASS_EXPLICIT, CUS_ATTRTYPE_EXP_PSK, 0,
-			 (strlen(GOOD_PSK) + 1), (uint8_t*) GOOD_PSK) < 0) {
-	fprintf(stderr, "ERROR %s: custos_updateAttr() failed\n", argv[0]);
-	return EXIT_FAILURE;
+                         CUS_ATTRCLASS_EXPLICIT, CUS_ATTRTYPE_EXP_PSK, 0,
+                         (strlen(GOOD_PSK) + 1), (uint8_t*) GOOD_PSK) < 0) {
+        fprintf(stderr, "ERROR %s: custos_updateAttr() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     /* Print Request */
     fprintf(stdout, "********* Third  Request *********\n");
     if(custos_printReq(req, 0, stdout) < 0) {
-	fprintf(stderr, "ERROR %s: custos_printReq() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_printReq() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     fprintf(stdout, "\n");
 
@@ -202,20 +202,20 @@ int main(int argc, char* argv[]) {
     fprintf(stdout, "******* Third  Request Json *******\n");
     reqjson = custos_ReqToJson(req);
     if(!reqjson) {
-	fprintf(stderr, "ERROR %s: custos_ReqToJson() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_ReqToJson() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     reqstr = json_object_to_json_string(reqjson);
     if(!reqstr) {
-	fprintf(stderr, "ERROR %s: json_object_to_json_string() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: json_object_to_json_string() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     fprintf(stdout, "%s\n", reqstr);
     fprintf(stdout, "\n");
     json_object_put(reqjson);
     reqjson = NULL;
 
-   /* Get Response - 3rd Attempt - Succeeds */
+    /* Get Response - 3rd Attempt - Succeeds */
     res = custos_getRes(req);
     if(!res) {
     	fprintf(stderr, "ERROR %s: custos_getRes() failed\n", argv[0]);
@@ -225,21 +225,21 @@ int main(int argc, char* argv[]) {
     /* Print Response */
     fprintf(stdout, "********* Third Response ********\n");
     if(custos_printRes(res, 0, stdout) < 0) {
-	fprintf(stderr, "ERROR %s: custos_printRes() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_printRes() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
     fprintf(stdout, "\n");
 
-   /* Free Response */
+    /* Free Response */
     if(custos_destroyRes(&res) < 0) {
-	fprintf(stderr, "ERROR %s: custos_destroyRes() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_destroyRes() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     /* Free Request */
     if(custos_destroyReq(&req) < 0) {
-	fprintf(stderr, "ERROR %s: custos_destroyReq() failed\n", argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr, "ERROR %s: custos_destroyReq() failed\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
